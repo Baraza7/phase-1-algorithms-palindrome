3ebfe98462b78dbe233516a82f332e793c35d2fd
baraza7 <barazabrian87@gmail.com>
2022-12-16 16:17:08 +0300

    final project

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+//explanation of your solution here**
+//This function first uses the split() method to split the word string into an array of individual characters. 
+//It then uses the reverse() method to reverse the order of the elements in the array,
+// and finally the join() method to join the array back into a single string.
+
+function reverse(word){
+  return word.split("").reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
+//The function below takes in a single string argument called word and returns a boolean value 
+//indicating whether or not the input word is a palindrome.
 
-/*
-  Add written explanation of your solution here
-*/
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
 
-// You can run `node index.js` to view these console logs
+  return word === reversedWord;
+}
+
+// The code below, function with different strings as arguments and logs the results to the console.
+ //The expected results are passed to console.log.
+
+Finally, the code exports the isPalindrome function as the value of module.exports, which allows it to be imported and used in other modules.
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +28,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
